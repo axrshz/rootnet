d68f2e56a88df4b894250f1592fee4c9bbec1a7c
axrshz <axrshz@gmail.com>
2025-10-31 08:39:22 +0530

    basic http server

---
+++ pkg/http/response.go
@@ -0,0 +1,58 @@
+package http
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/axrshz/rootnet/pkg/http/status"
+)
+
+type Response struct {
+	Version    string
+	StatusCode int
+	StatusText string
+	Headers    map[string]string
+	Body       []byte
+}
+
+func NewResponse() *Response {
+	return &Response{
+		Version: "HTTP/1.1",
+		Headers: make(map[string]string),
+	}
+}
+
+func FormatResponse(r *Response) []byte {
+	var builder strings.Builder
+
+	// Status line
+	builder.WriteString(fmt.Sprintf("%s %d %s\r\n", r.Version, r.StatusCode, r.StatusText))
+
+	// Headers
+	for key, value := range r.Headers {
+		builder.WriteString(fmt.Sprintf("%s: %s\r\n", key, value))
+	}
+
+	// Empty line
+	builder.WriteString("\r\n")
+
+	// Body
+	return append([]byte(builder.String()), r.Body...)
+}
+
+func StatusText(code int) string {
+	return status.Text(code)
+}
+
+func (r *Response) SetStatus(code int) {
+    r.StatusCode = code
+    r.StatusText = status.Text(code)
+}
+
+func (r *Response) SetHeader(key, value string) {
+    r.Headers[key] = value
+}
+
+func (r *Response) SetBody(body []byte) {
+    r.Body = body
+}
